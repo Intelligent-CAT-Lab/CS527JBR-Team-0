5c80f7ea0dc3d6afd5f9a27c12a52d4324b92fdb
JackyYang258 <yang1213853@gmail.com>
2026-02-06 23:23:56 -0600

    Initial commit

---
+++ milestone2/milestone2.py
@@ -0,0 +1,18 @@
+def count_tool_use(model_name: str, instance_id: str) -> dict: # This cannot be changed
+  pass
+
+def locate_navigation(model_name: str, instance_id: str) -> list[int]: # This cannot be changed
+  pass
+
+def locate_generated_tests(model_name: str, instance_id: str) -> dict: # This cannot be changed
+  pass
+
+def count_fail_to_pass(model_name: str, instance_id: str) -> dict: # This cannot be changed
+  pass
+
+def plot_violin(): # You can change the signature of this function if you want to pass in arguments
+  pass
+
+if __name__ == "__main__":
+  # You can add code here to call the above functions and print their results for testing purposes
+  pass
