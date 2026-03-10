f0f3da3477e4b1af9fe5ee7cbd21654b54ccaf9a
topgunlcs98 <ericliu9866@gmail.com>
2026-03-10 13:26:09 -0500

    add milestone 3

---
+++ milestone3/README.md
@@ -0,0 +1,5 @@
+## Task 2 Report
+
+## Task 3 Report
+
+## Task 4 Report
---
+++ milestone3/milestone3.py
@@ -0,0 +1,5 @@
+def collect_graph_metrics(model_name: str, instance_id: str) -> dict:
+    pass
+
+def detect_inefficiency_patterns(model_name: str, instance_id: str) -> dict:
+    pass
